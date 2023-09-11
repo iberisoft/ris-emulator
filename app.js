d5477b99c6c9d32b4ac5dde7bf6d77cab047512a
Pavel Zaytsev <pavel@iberisoft.com>
2023-09-11 17:06:05 +0200

    Add project files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+const port = 5200;
+
+app.post("/", (request, response) => {
+    console.log(`PACS has created study ${request.body.StudyInstanceUid}`);
+    response.end();
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
