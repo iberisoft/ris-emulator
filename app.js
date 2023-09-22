be79338c79860e0f791f8bb9e4a2f3afa82911a6
Pavel Zaytsev <pavel@iberisoft.com>
2023-09-22 17:21:30 +0200

    Support JWT

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
 const port = 5200;
+const secretKey = 'mysecretkey';
+const userName = 'admin';
+
+app.get('/token', (_, response) => {
+    const token = jwt.sign({ userName }, secretKey);
+    response.json(token);
+});
 
 app.post('/', (request, response) => {
-    console.log(`PACS has created study ${request.body.StudyInstanceUid}`);
+    if (authorize(request) == userName) {
+        console.log(`PACS has created study ${request.body.StudyInstanceUid}`);
+    }
+    else {
+        response.statusCode = 401;
+    }
     response.end();
 });
+
+function authorize(request) {
+    const token = request.headers.authorization.split(' ')[1];
+    if (token) {
+        return jwt.verify(token, secretKey).userName;
+    }
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
